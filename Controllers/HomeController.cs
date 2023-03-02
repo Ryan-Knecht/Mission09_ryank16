6c4e2f14a7132ef790da608861b8d10b04bf102f
Ryan-Knecht <ryank16@byu.edu>
2023-03-01 22:06:01 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = repo.Books.ToList();
+            return View(blah);
+        }
+
+        
+    }
+}
---
+++ Models/IBookstoreRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public interface IBookstoreRepository 
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
